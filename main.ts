13fde3bf5ed8d8b7522f650806344c8a8937ee04
Ezequiel <ezequielgaribottovillanueva@gmail.com>
2024-10-28 15:55:09 +0100

    sensor temperatura y luminosidad

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+let luminosidad = 0
+let temperatura = 0
+input.onButtonPressed(Button.A, function () {
+    luminosidad = input.lightLevel()
+    if (luminosidad > 150) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+    } else {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.InBackground)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    temperatura = input.temperature()
+    if (temperatura > 22) {
+        basic.showIcon(IconNames.Pitchfork)
+    } else {
+        basic.showIcon(IconNames.Chessboard)
+    }
+})
 basic.forever(function () {
 	
 })
