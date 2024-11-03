cd0ec25346252a0e2e36226aaa94e9e21105d22c
Ezequiel <ezequielgaribottovillanueva@gmail.com>
2024-11-03 22:05:54 +0100

    for ever

--- main.ts
+++ main.ts
@@ -1,28 +1,27 @@
-let luminosidad = 0
+let isTemperatura = false
 let temperatura = 0
+let luminosidad = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    luminosidad = input.lightLevel()
-    if (luminosidad > 150) {
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
-    } else {
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.InBackground)
-    }
+    isTemperatura = false
 })
 input.onButtonPressed(Button.B, function () {
-    temperatura = input.temperature()
-    if (temperatura > 22) {
-        basic.showIcon(IconNames.Pitchfork)
-    } else {
-        basic.showIcon(IconNames.Chessboard)
-    }
+    isTemperatura = true
 })
 basic.forever(function () {
-	
+    if (isTemperatura == true) {
+        music.stopMelody(MelodyStopOptions.All)
+        temperatura = input.temperature()
+        if (temperatura > 22) {
+            basic.showIcon(IconNames.Pitchfork)
+        } else {
+            basic.showIcon(IconNames.Chessboard)
+        }
+    } else {
+        luminosidad = input.lightLevel()
+        if (luminosidad > 150) {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Entertainer), music.PlaybackMode.InBackground)
+        } else {
+            music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.InBackground)
+        }
+    }
 })
